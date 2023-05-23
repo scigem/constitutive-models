e29b003a068675dd1c18476ed2cf2e8329262b6d
Benjamin Marks <benjy.marks@sydney.edu.au>
2023-05-23 11:00:33 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,133 @@
+import Plotly from 'plotly.js-dist';
+import css from './main.css';
+
+let e_v_vec, e_a_vec, p_vec, q_vec;
+
+function linear_elasticity(de_v,de_a){
+    let K = parseFloat(E.value)*1e6/(3*(1-2*parseFloat(nu.value))); // convert from GPa to kPa
+    let G = parseFloat(E.value)*1e6/(2*(1+parseFloat(nu.value))); // convert from GPa to kPa
+    let dp = K*de_v;
+    let dq = 2*G*de_a;
+    // console.log(dp, dq);
+    return [dp, dq]
+}
+
+function drucker_prager(de_v,de_a){
+    // NOTE: THIS IS TOTALLY BROKEN, JUST A SUGGESTION FOR HOW TO IMPLEMENT THINGS
+    let K = E.value*1e6/(3*(1-2*nu.value)); // convert from GPa to kPa
+    let G = E.value*1e6/(2*(1+nu.value)); // convert from GPa to kPa
+    let p = p_vec[p_vec.length - 1];
+    let q = q_vec[q_vec.length - 1];
+    let e_v = e_v_vec[e_v_vec.length - 1];
+    let e_a = e_a_vec[e_a_vec.length - 1];
+    let lambda = (3*G*p*q*e_v - K*q*q*e_v)/(3*G*mu.value*p*p + K*beta.value*q*q);
+    if ( isNaN(lambda) ) { lambda = 0 }
+    if ( lambda < 0 ) { lambda = 0 }
+    let dp = K*(de_v + beta.value*lambda*(q/mu.value/p)**s.value);
+    let dq = 2*G*(e_a - 3/2*lambda*(q/mu.value/p)**(s.value-1));
+    return [dp, dq]
+    
+}
+
+function max(de_v,de_a){
+    let dp = 0;
+    let dq = 0;
+    return [dp, dq]
+}
+
+function store(dp,dq,de_v,de_a){
+    e_v_vec.push(e_v_vec[e_v_vec.length - 1] + de_v);
+    e_a_vec.push(e_a_vec[e_a_vec.length - 1] + de_a);
+    p_vec.push(p_vec[p_vec.length - 1] + dp);
+    q_vec.push(q_vec[q_vec.length - 1] + dq);
+}
+
+function time_march(m) {
+    let dp, dq;
+    e_v_vec = [0];
+    e_a_vec = [0]
+    p_vec = [1e-5];
+    q_vec = [1e-5];
+
+    let de_v = 1e-3;
+    let de_a = 0;
+    for (let i=0; i<10; i++){
+        [dp, dq] = m(de_v,de_a);
+        store(dp,dq,de_v,de_a);
+    }
+    de_v = 0;
+    de_a = 1e-3;
+    for (let i=0; i<10; i++){
+        [dp, dq] = m(de_v,de_a);
+        store(dp,dq,de_v,de_a);
+    }
+
+    
+}
+
+function update() {
+    let current_model = model.options[model.selectedIndex].value;
+    if ( current_model === 'linear_elasticity' ) {
+        document.getElementById('E_div').hidden=false ;
+        document.getElementById('nu_div').hidden=false ;
+        document.getElementById('beta_div').hidden=true ;
+        document.getElementById('mu_div').hidden=true ;
+        document.getElementById('s_div').hidden=true ;
+    } else if ( current_model === 'drucker_prager' ) {
+        document.getElementById('E_div').hidden=false ;
+        document.getElementById('nu_div').hidden=false ;
+        document.getElementById('beta_div').hidden=false ;
+        document.getElementById('mu_div').hidden=false ;
+        document.getElementById('s_div').hidden=false ;
+    } else if ( current_model === 'max' ) {
+        document.getElementById('E_div').hidden=false ;
+        document.getElementById('nu_div').hidden=false ;
+        document.getElementById('beta_div').hidden=true ;
+        document.getElementById('mu_div').hidden=false ;
+        document.getElementById('s_div').hidden=false ;
+    }
+
+    time_march(eval(current_model)); // get reference to the function that defines the constitutive model
+    draw_graphs();
+}
+
+
+function draw_graphs(){
+    let trace1 = {
+        x: e_v_vec,
+        y: q_vec,
+    };
+    let layout = {
+        xaxis: {
+            title: 'Axial strain (%)',
+        },
+        yaxis: {
+            title: 'Deviatoric stress (kPa)',
+        }
+    }
+
+    Plotly.react('graph_1', [trace1], layout);
+
+    let trace2 = {...trace1};
+    trace2.x = p_vec;
+    trace2.y = q_vec;
+    layout.xaxis.title = 'Pressure (kPa)';
+    layout.yaxis.title = 'Deviatoric stress (kPa)';
+
+    Plotly.react('graph_2', [trace2], layout);
+
+    let trace3 = {...trace1 };
+    trace3.x = e_a_vec;
+    trace3.y = e_v_vec;
+    layout.xaxis.title = 'Axial strain (%)';
+    layout.yaxis.title = 'Volumetric strain (%)';
+
+    Plotly.react('graph_3', [trace3], layout);
+}
+
+update();
+
+var elements = document.getElementsByClassName("updater");
+Array.from(elements).forEach(function(element) {
+    element.addEventListener('input', update);
+});
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require("webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+  {
+    mode: "development",
+    // mode: "production",
+    entry: {
+      "index": './index.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Constitutive model',
+        // favicon: "./resources/favicon512.png",
+        template: "index.html",
+        filename: "index.html",
+        chunks: ['index']
+      })
+    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name]-bundle.js',
+      clean: true,
+    },
+    devServer: {
+      static: {
+        directory: './dist'
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(json|png|svg|jpg|jpeg|gif|mp3|stl|glb)$/i,
+          type: 'asset/resource',
+          use: ["file-loader?name=[name].[ext]"]
+        },
+      ],
+    },
+  },
+];
